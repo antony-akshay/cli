3fa6f6359f22a3db9bae267353d2b9f5e22dbfe4
antony-akshay <akshay.dev.ac.in@gmail.com>
2026-03-14 16:36:04 +0530

    refactor::color_ls

--- color_ls/src/main.rs
+++ color_ls/src/main.rs
@@ -1,44 +1,76 @@
 use clap::Parser;
-use std::{fs, path::{Path, PathBuf}};
 use owo_colors::OwoColorize;
+use std::{
+    fs::{self, File},
+    path::{Path, PathBuf},
+};
+use strum::Display;
 
-#[derive(Debug,Parser)]
-#[command(version,)]
-struct Cli{
-    path:Option<PathBuf>
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Cli {
+    path: Option<PathBuf>,
+}
+
+#[derive(Debug, Display)]
+enum EntryType {
+    File,
+    Dir,
+}
+
+#[derive(Debug)]
+struct FileEntry {
+    name: String,
+    e_type: EntryType,
+    len_bytes: u64,
+    modified: String,
 }
 
 fn main() {
-    let cli=Cli::parse();
+    let cli = Cli::parse();
 
     let path = cli.path.unwrap_or(PathBuf::from("."));
 
-    if let Ok(does_exist) = fs::exists(&path){
+    if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
-            for file in get_files(&path){
-                println!("{}",file);
+            for file in get_files(&path) {
+                println!("{:?}", file);
             }
-        }else{
-            println!("{}","Path does not exist".red());
+        } else {
+            println!("{}", "Path does not exist".red());
         }
-    }else{
-        println!("{}","error reading directory".red());
+    } else {
+        println!("{}", "error reading directory".red());
     }
 }
 
-
-fn get_files(path:&Path) -> Vec<String>{
+fn get_files(path: &Path) -> Vec<FileEntry> {
     let mut data = Vec::default();
-    
-    if let Ok(read_dir) = fs::read_dir(path){
+
+    if let Ok(read_dir) = fs::read_dir(path) {
         for entry in read_dir {
-            if let Ok(file) = entry{
-                data.push(
-                    file.file_name()
-                    .into_string().unwrap_or("unknown name".into())
-                );
+            if let Ok(file) = entry {
+                map_data(file, &mut data);
             }
         }
     }
     data
+}
+
+fn map_data(file: fs::DirEntry, data: &mut Vec<FileEntry>) {
+    if let Ok(meta) = fs::metadata(&file.path()) {
+        data.push(FileEntry {
+            name: file
+                .file_name()
+                .into_string()
+                .unwrap_or("unknown name".into()),
+            e_type: if meta.is_dir() {
+                EntryType::Dir
+            } else {
+                EntryType::File
+            },
+            len_bytes: meta.len(),
+            modified: "".to_string(),
+        });
+    }
 }
