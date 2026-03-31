0be206f3f26a4a2514dea2916295bbbfc88dafe5
antony-akshay <akshay.dev.ac.in@gmail.com>
2026-03-31 20:02:20 +0530

    roulette:death_animation

--- roulette/src/main.rs
+++ roulette/src/main.rs
@@ -1,21 +1,33 @@
-use rand::Rng;
 use rand::RngExt;
 use rand::rng;
+use std::io;
 
 fn main() {
     let mut rng = rng();
-    let bullet: i32 = rng.random_range(1..=5);
-
+    let bullet: i32 = rng.random_range(1..=6);
     println!("{}", bullet);
 
     for i in 1..=5 {
-        println!("Pull the trigger");
+        println!("🔫");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
 
         if bullet == i {
-            println!("💀 Dead");
-            break;
-        } else {
-            println!("😌 Safe");
+            death_animation();
+            return;
         }
     }
+    println!("Allah is with you");
+}
+
+fn death_animation() {
+    println!(r#"
+    ☠️  YOU DIED ☠️
+    
+    ──────▄▀▄─────▄▀▄
+    ─────▄█░░▀▀▀▀▀░░█▄
+    ─▄▄──█░░░░░░░░░░░█──▄▄
+    █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█
+    "#
+    );
 }
